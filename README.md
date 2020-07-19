46ece083e38f56962d9e01aefa4315f33b098e3c
Madhav Sharma <shekharmadhav03@gmail.com>
2020-07-19 15:14:14 +0530

    ex1

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 
 - [Practice makes Perfect](https://github.com/pseudocodenerd/e-lite-section-2/tree/master/ex0)
 
+## Exercise 1
+
+- [Filter Question Form](https://ex1-eight.vercel.app)
+
 ## Exercise 2
 
 - [Numbers API](https://ex2.vercel.app)
---
+++ ex1/script.js
@@ -0,0 +1,72 @@
+// Learnt about dependent forms, ternary operator, find and every methods.
+// Initial reading from https://stackoverflow.com/questions/29623225/javascript-dependent-drop-down-list
+
+const name_inp = document.querySelector("input#name")
+const email_inp = document.querySelector("input#email")
+const class_sel = document.querySelector("select#class")
+const sec_sel = document.querySelector("select#section")
+const form = document.querySelector("form")
+
+const sec_num = {
+    "VI": 10,
+    "VII": 10,
+    "VIII": 10,
+    "IX": 15,
+    "X": 10,
+    "XI": 24,
+    "XII": 25
+}
+
+function get_sec_list(n) {
+    const alph = "abcdefghijklmnopqrstuvwxyz".split("")
+    let sec_list = []
+    for (let i = 0; i < n; i++) {
+        sec_list.push(alph[i].toUpperCase())
+    }
+    return sec_list
+}
+
+// Dependent section dropdown on selected class
+
+class_sel.addEventListener("change", event => {
+    if (event.target.value) {
+        // console.log(1)
+        sec_sel.disabled = false
+        let foo = get_sec_list(sec_num[event.target.value])
+        for (let i = 0; i < foo.length; i++) {
+            let sec = document.createElement("option")
+            // console.log(foo[i].toUpperCase())
+            sec.innerHTML = `<option key="${foo[i]}">${foo[i]}</option>`
+            sec_sel.appendChild(sec)
+        }
+    } else {
+        sec_sel.disabled = true
+        sec_sel.innerHTML = `<option key="" selected>Select a section</option>`
+    }
+})
+
+
+// Validation
+function validate(name, email) {
+    const rkpRegex = /^[r|e|v]\d{4,8}.+@dpsrkp.net$/
+    const isValidName = (name != "")
+    const isValidMail = rkpRegex.test(email)
+    let validated = isValidMail && isValidName
+    if (!isValidMail) {
+        alert("Please enter your valid student email-id")
+    }
+    if (!isValidName) {
+        alert("Name cannot be empty. Please try again.")
+    }
+    if (validated) {
+        alert(`Hello, ${name} <${email}> from ${class_sel.value}-${sec_sel.value}.`)
+    }
+    // console.log(isValidName, isValidMail)
+}
+
+function submit_form(event) {
+    event.preventDefault()
+    validate(name_inp.value, email_inp.value)
+}
+
+form.addEventListener("submit", submit_form)
